d9326e5fc75f8d10c0cae25d7da8c5a8edaad60b
lvheng <1834805770@qq.com>
2021-03-25 21:58:28 +0800

    匹配任意文件，处理不存在的文件(404)

--- index.ts
+++ index.ts
@@ -3,41 +3,43 @@ import {IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 import * as p from "path";
 import * as url from "url";
+import { URL } from "url"
 
 const server = http.createServer();
 // relative 表示拼接两个路径， __dirname: 表示当前文件绝对路径
-const publicDir = p.resolve(__dirname, 'public')
+const publicDir = p.resolve(__dirname, 'public');
 
 // 每次请求，都会触发箭头函数
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  const { method, url: path, headers } = request
-  const { pathname, search } = url.parse(path) // 获取不带查询参数的路径，及查询参数
-
-  switch(pathname) {
-    case '/index.html':
-      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data)=>{
-        if (error) throw error
-        response.end(data.toString())
-      })
-      break;
-    case '/style.css':
-      response.setHeader('Content-Type', 'text/css; charset=utf-8');
-      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data)=>{
-        if (error) throw error
-        response.end(data.toString())
-      })
-      break;
-    case '/main.js':
-      response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data)=>{
-        if (error) throw error
-        response.end(data.toString())
-      })
-      break;
-    default:
-      response.statusCode = 404
-      response.end()
+  const {method, url: path, headers} = request;
+  // const myURL = new URL(path)
+  // console.log(myURL.hash)
+  const {pathname, search} = url.parse(path); // 获取不带查询参数的路径，及查询参数
+  let filename = pathname.substr(1);
+  // response.setHeader('Content-Type', 'text/css; charset=utf-8');
+  if (filename === '') {
+    filename = 'index.html'
   }
+  fs.readFile(p.resolve(publicDir, filename), (error, data) => {
+    if (error) {
+      if (error.errno === -4058) {
+        response.statusCode = 404;
+        fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
+          response.end(data);
+        });
+      } else if (error.errno === -4068) {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.statusCode = 403;
+        response.end('没有权限访问该目录');
+      } else {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.statusCode = 500;
+        response.end('服务器烦啊，请稍后再试');
+      }
+    } else {
+      response.end(data);
+    }
+  });
 });
 
 // 监听本地 8888 端口
