099c85f7b852ab4dd1f617a05b0e6b8f6cd17042
ChenNing02 <2633565885@qq.com>
2021-03-25 11:10:48 +0800

    根据 url 返回不同文件

--- index.ts
+++ index.ts
@@ -1,33 +1,41 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
+import * as fs from "fs";
+import * as p from "path";
 
 const server = http.createServer();
+// relative 表示拼接两个路径， __dirname: 表示当前文件绝对路径
+const publicDir = p.resolve(__dirname, 'public')
+console.log('publicDir')
+console.log(publicDir)
 
 // 每次请求，都会触发箭头函数
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  console.log('request.method');
-  console.log(request.method);
-  console.log('request.url');
-  console.log(request.url);
-  console.log('request.headers');
-  console.log(request.headers);
-
-  const array = []
-  request.on('data', (chunk) => {
-    array.push(chunk)
-  })
-  request.on('end', () => {
-    const body = Buffer.concat(array).toString()
-    console.log('body')
-    console.log(body);
-
-    response.statusCode = 404
-    response.setHeader('T-avengers', `I'm Iron Man`)
-    response.write('1\n')
-    response.write('2\n')
-    response.write('3\n')
-    response.end(); // 结束相应
-  })
+  const { method, url, headers } = request
+  console.log('# url')
+  console.log(url)
+  switch(url) {
+    case '/index.html':
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data)=>{
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break;
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8');
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data)=>{
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break;
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data)=>{
+        if (error) throw error
+        response.end(data.toString())
+      })
+      break;
+  }
 });
 
 // 监听本地 8888 端口
---
+++ public/main.js
@@ -0,0 +1,5 @@
+const h2 = document.querySelector('h2')
+
+h2.onclick = () => {
+  alert('我是H2')
+}
