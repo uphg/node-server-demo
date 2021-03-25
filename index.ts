95b3cea5a147e112fcd409a931f8d12c44ccffcd
ChenNing02 <2633565885@qq.com>
2021-03-25 12:24:23 +0800

    处理查询参数

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 import * as p from "path";
+import * as url from "url";
 
 const server = http.createServer();
 // relative 表示拼接两个路径， __dirname: 表示当前文件绝对路径
@@ -9,9 +10,10 @@ const publicDir = p.resolve(__dirname, 'public')
 
 // 每次请求，都会触发箭头函数
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  const { method, url, headers } = request
-  
-  switch(url) {
+  const { method, url: path, headers } = request
+  const { pathname, search } = url.parse(path) // 获取不带查询参数的路径，及查询参数
+
+  switch(pathname) {
     case '/index.html':
       fs.readFile(p.resolve(publicDir, 'index.html'), (error, data)=>{
         if (error) throw error
@@ -32,6 +34,9 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
         response.end(data.toString())
       })
       break;
+    default:
+      response.statusCode = 404
+      response.end()
   }
 });
 
