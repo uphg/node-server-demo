4854e59640a192c6f17b873347eb678c0e7d3049
lvheng <1834805770@qq.com>
2021-03-27 13:05:12 +0800

    格式化代码

--- index.ts
+++ index.ts
@@ -2,27 +2,27 @@ import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 import * as p from "path";
-import { URL } from "url";
+import {URL} from "url";
 
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, 'public'); // relative 表示拼接两个路径， __dirname: 表示当前文件绝对路径
 let cacheAge = 3600 * 24 * 365; // 将文件缓存时间调至 1 年
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  const { method, url: path, headers} = request;
+  const {method, url: path, headers} = request;
 
   // 此处参考：https://nodejs.org/docs/latest-v12.x/api/http.html#http_message_url
-  const { pathname, search } = new URL(path, `http://${headers.host}`)
+  const {pathname, search} = new URL(path, `http://${headers.host}`);
 
   if (method !== 'GET') {
-    response.statusCode = 405
-    response.end('这是一个假响应')
+    response.statusCode = 405;
+    response.end('这是一个假响应');
     return;
   }
 
   let filename = pathname.substr(1);
   if (filename === '') {
-    filename = 'index.html'
+    filename = 'index.html';
   }
   fs.readFile(p.resolve(publicDir, filename), (error, data) => {
     if (error) {
@@ -40,7 +40,7 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
         response.end('服务器烦啊，请稍后再试');
       }
     } else {
-      response.setHeader('Cache-Control', `public, max-age=${cacheAge}`)
+      response.setHeader('Cache-Control', `public, max-age=${cacheAge}`);
       response.end(data);
     }
   });
