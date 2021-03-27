535c03e9d93d71cdd1d31840495a68cbcb4f1163
lvheng <1834805770@qq.com>
2021-03-27 11:23:15 +0800

    弃用url.parse(path)的方式获取路径，改用new URL()

--- index.ts
+++ index.ts
@@ -2,19 +2,17 @@ import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
 import * as fs from "fs";
 import * as p from "path";
-import * as url from "url";
-import { URL } from "url"
+import { URL } from "url";
 
 const server = http.createServer();
-// relative 表示拼接两个路径， __dirname: 表示当前文件绝对路径
-const publicDir = p.resolve(__dirname, 'public');
-let cacheAge = 3600 * 24 * 100;
-// 每次请求，都会触发箭头函数
+const publicDir = p.resolve(__dirname, 'public'); // relative 表示拼接两个路径， __dirname: 表示当前文件绝对路径
+let cacheAge = 3600 * 24 * 365; // 将文件缓存时间调至 1 年
+
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  const {method, url: path, headers} = request;
-  // const myURL = new URL(path)
-  // console.log(myURL.hash)
-  const {pathname, search} = url.parse(path); // 获取不带查询参数的路径，及查询参数
+  const { method, url: path, headers} = request;
+
+  // 此处参考：https://nodejs.org/docs/latest-v12.x/api/http.html#http_message_url
+  const { pathname, search } = new URL(path, `http://${headers.host}`)
 
   if (method !== 'GET') {
     response.statusCode = 405
@@ -23,23 +21,21 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
   }
 
   let filename = pathname.substr(1);
-  // response.setHeader('Content-Type', 'text/css; charset=utf-8');
   if (filename === '') {
     filename = 'index.html'
   }
   fs.readFile(p.resolve(publicDir, filename), (error, data) => {
     if (error) {
+      response.setHeader('Content-Type', 'text/html; charset=utf-8');
       if (error.errno === -4058) {
         response.statusCode = 404;
         fs.readFile(p.resolve(publicDir, '404.html'), (error, data) => {
           response.end(data);
         });
       } else if (error.errno === -4068) {
-        response.setHeader('Content-Type', 'text/html; charset=utf-8');
         response.statusCode = 403;
         response.end('没有权限访问该目录');
       } else {
-        response.setHeader('Content-Type', 'text/html; charset=utf-8');
         response.statusCode = 500;
         response.end('服务器烦啊，请稍后再试');
       }
